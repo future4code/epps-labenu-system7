2cc047beed0a5fee9e8298ba72a634540515057e
Yuri Pinheiro <yuripinheirot@gmail.com>
2021-03-30 16:47:40 -0400

    Iniciando projeto

---
+++ src/app.ts
@@ -0,0 +1,12 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+const port = 3003;
+
+app.use(express.json());
+app.use(cors());
+
+app.listen(port, () => {
+  console.log(`App is running on port ${port}`);
+});
